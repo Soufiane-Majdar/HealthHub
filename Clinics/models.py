2273b2ff0b6977b28ac2e5aa8a027d43681940c3
Soufiane-Majdar <soufianemj3145@gmail.com>
2023-05-18 18:29:28 +0200

    fill some data and testing it

--- Clinics/models.py
+++ Clinics/models.py
@@ -11,8 +11,7 @@ class Clinique(models.Model):
     image = models.ImageField(upload_to='clinique_images/')
     tele = models.CharField(max_length=20)
     email = models.EmailField()
-    website = models.URLField()
-    location = models.CharField(max_length=255)
+
 
     def __str__(self):
         return self.nom
--- Clinics/views.py
+++ Clinics/views.py
@@ -2,6 +2,18 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+from .models import Clinique
+
 
 def home(request):
+    cliniques = Clinique.objects.all()
+    for clinique in cliniques:
+        print("============================================================================\n\n")
+        print("Nom:", clinique.nom)
+        print("Adresse:", clinique.adresse)
+        print("Description:", clinique.description)
+        print("Image:", clinique.image.url)  # Assuming image field is not null
+        print("Tele:", clinique.tele)
+        print("Email:", clinique.email)
+
     return render(request, 'index.html')
